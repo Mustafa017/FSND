cfb1ca4725c03a82b99cc67c4278a44a7bfa3ae3
Mustafa Khamisi <mustafangure95@gmail.com>
2021-01-12 08:46:54 -0600

    fix: Change Payload parameters

--- projects/02_trivia_api/starter/frontend/src/components/QuizView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuizView.js
@@ -119,7 +119,10 @@ class QuizView extends Component {
                 value={id}
                 className="play-category"
                 onClick={() =>
-                  this.selectCategory({ type: this.state.categories[id], id })
+                  this.selectCategory({
+                    type: this.state.categories[id]["type"],
+                    id: this.state.categories[id]["id"],
+                  })
                 }
               >
                 {this.state.categories[id]["type"]}
