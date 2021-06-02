03fd7548c3f77b5754f7879ac9629e8b2cd04ae4
Mustafa Khamisi <mustafangure95@gmail.com>
2021-06-02 12:31:48 -0500

    fix: Indicate completed tasks

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,6 @@
 import os
+
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,6 +11,6 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
+# DONE IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Galacticos$1@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
