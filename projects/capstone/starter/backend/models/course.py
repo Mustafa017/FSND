bdd925661aa628b5e77ce08bd571c663913e6b85
Mustafa Khamisi <mustafangure95@gmail.com>
2021-08-06 23:07:02 -0500

    fix: Alter column type crs_desc to text

--- projects/capstone/starter/backend/models/course.py
+++ projects/capstone/starter/backend/models/course.py
@@ -6,7 +6,7 @@ class Course(db.Model):
     crs_id = db.Column(db.Integer, primary_key=True)
     crs_code = db.Column(db.String(20), unique=True, nullable=False)
     crs_title = db.Column(db.String(50), unique=True, nullable=False)
-    crs_desc = db.Column(db.String(255), nullable=False)
+    crs_desc = db.Column(db.Text, nullable=False)
     crs_book = db.Column(db.String(255))
 
     def __init__(self, code, title, desc, book) -> None:
