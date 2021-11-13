4dbc049663f21752c57415028565d9401fca4dbb
Mustafa Khamisi <mustafangure95@gmail.com>
2021-11-13 07:30:08 -0600

    fix: Remove class keyword as backref value

--- projects/capstone/starter/backend/models/classroom.py
+++ projects/capstone/starter/backend/models/classroom.py
@@ -6,7 +6,7 @@ class Classroom(db.Model):
     cls_id = db.Column(db.Integer, primary_key=True)
     cls_code = db.Column(db.String(20), unique=True, nullable=False)
     cls_size = db.Column(db.String(50), unique=True, nullable=False)
-    courses = db.relationship('Course', backref='class', lazy=True)
+    courses = db.relationship('Course', backref='classroom', lazy=True)
 
     def __init__(self, code, size) -> None:
         self.cls_code = code
