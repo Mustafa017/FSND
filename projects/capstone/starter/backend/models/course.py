4d2ddbc127164ecb8597c6dd42d67ba2c1e3f0c6
Mustafa Khamisi <mustafangure95@gmail.com>
2021-11-15 13:27:25 -0600

    feat: Add insert method to the course class

--- projects/capstone/starter/backend/models/course.py
+++ projects/capstone/starter/backend/models/course.py
@@ -13,15 +13,20 @@ class Course(db.Model):
     crs_cls_id = db.Column(db.Integer, db.ForeignKey(
         'classroom.cls_id'), nullable=False)
 
-    def __init__(self, code, title, desc, book) -> None:
+    def __init__(self, code, title, desc, book, lec, room) -> None:
         self.crs_code = code
         self.crs_title = title
         self.crs_desc = desc
         self.crs_book = book
+        self.crs_lec_id = lec
+        self.crs_cls_id = room
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
     def __repr__(self) -> str:
-        return f'id:{self.crs_id}, code:{self.crs_code}, \
-            title:{self.crs_title}'
+        return f'<Course:{self.crs_id} {self.crs_title}>'
 
     @property
     def format(self):
