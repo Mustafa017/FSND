a67b8a6171d092225f23c67405267280920d6116
Mustafa Khamisi <mustafangure95@gmail.com>
2021-11-15 13:25:31 -0600

    feat: Create route to add new course

--- projects/capstone/starter/backend/app.py
+++ projects/capstone/starter/backend/app.py
@@ -1,11 +1,10 @@
-from re import T
 import os
 import sys
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from flask_migrate import current
 import config
-from auth.auth import require_auth
+from auth.auth import authorize_user
 from models import setup_db, Student, Course, Classroom, Enrollments,\
     Lecturer
 
@@ -19,9 +18,10 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        return "School Management APP"
+        return "Course Management APP"
 
     @app.route('/students')
+    @authorize_user('get:data')
     def get_students():
         items_limit = request.args.get('limit', 3, type=int)
         page_num = request.args.get('page', 1, type=int)
@@ -43,6 +43,7 @@ def create_app(test_config=None):
             raise sys.exc_info()[1]
 
     @app.route('/students/<int:student_id>')
+    @authorize_user('get:data')
     def get_student_by_id(student_id):
         try:
             student = db.session.query(Student).filter(
@@ -59,6 +60,7 @@ def create_app(test_config=None):
             raise sys.exc_info()[1]
 
     @app.route('/courses')
+    @authorize_user('get:data')
     def getCourses():
         try:
             courses = db.session.query(Course).order_by(Course.crs_id).all()
@@ -72,7 +74,37 @@ def create_app(test_config=None):
             # print(sys.exc_info())
             raise sys.exc_info()[1]
 
-    @app.errorhandler(404)
+    @app.route('/new_course', methods=['POST'])
+    def post_course():
+        body = request.get_json()
+        crs_title = body.get('title')
+        crs_code = body.get('code')
+        crs_desc = body.get('desc')
+        crs_book = body.get('book')
+        crs_lec = int(body.get('lec'))
+        crs_room = int(body.get('room'))
+
+        try:
+            # instantiate course object
+            crs = Course(crs_code, crs_title, crs_desc,
+                         crs_book, crs_lec, crs_room)
+            crs.insert()
+            courses = Course.query.order_by(Course.crs_id).all()
+            courses_list = [course.format for course in courses]
+
+            return jsonify({
+                'success': True,
+                'created': crs.crs_id,
+                'courses': courses_list,
+                'total': len(courses)
+            })
+        except Exception as e:
+            db.session.rollback()
+            abort(422)
+            # print(e)
+            # print(sys.exc_info()[1])
+
+    @ app.errorhandler(404)
     def not_found(error):
         return jsonify({
             "Success": False,
@@ -80,7 +112,7 @@ def create_app(test_config=None):
             "message": "Resource Not Found"
         }), 404
 
-    @app.errorhandler(500)
+    @ app.errorhandler(500)
     def server_error(error):
         return jsonify({
             "Success": False,
@@ -88,7 +120,7 @@ def create_app(test_config=None):
             "message": "Internal Server error"
         }), 500
 
-    @app.errorhandler(422)
+    @ app.errorhandler(422)
     def unprocessable(error):
         return jsonify({
             "Success": False,
