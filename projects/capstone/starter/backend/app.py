b53cccf9f2a21c79832fdd31f381e71b63493b56
Mustafa Khamisi <mustafangure95@gmail.com>
2021-11-17 10:44:39 -0600

    feat: add update, patch, and delete routes

--- projects/capstone/starter/backend/app.py
+++ projects/capstone/starter/backend/app.py
@@ -1,3 +1,4 @@
+from logging import error
 import os
 import sys
 from flask import Flask, request, abort, jsonify
@@ -20,62 +21,187 @@ def create_app(test_config=None):
     def index():
         return "Course Management APP"
 
+    #---------------------------------------------------------#
+    # Students
+    #---------------------------------------------------------#
+
     @app.route('/students')
     @authorize_user('get:data')
     def get_students():
         items_limit = request.args.get('limit', 3, type=int)
         page_num = request.args.get('page', 1, type=int)
         current_index = page_num - 1
+        error = False
+
         try:
             students = db.session.query(Student).order_by(Student.std_id).limit(
                 items_limit).offset(items_limit * current_index).all()
 
-            if students == 'undefined':
-                abort(404)
-            else:
-                formatted_std = [student.format for student in students]
-                return jsonify({
-                    "success": True,
-                    "data": formatted_std,
-                    "total_students": len(formatted_std)
-                })
+            formatted_std = [student.format for student in students]
+
         except Exception:
-            raise sys.exc_info()[1]
+            error = True
+            print(sys.exc_info())
+
+        if error:
+            abort(404)
+        else:
+            return jsonify({
+                "success": True,
+                "data": formatted_std,
+                "total_students": len(formatted_std)
+            })
 
     @app.route('/students/<int:student_id>')
     @authorize_user('get:data')
     def get_student_by_id(student_id):
+        error = False
+
         try:
             student = db.session.query(Student).filter(
                 Student.std_id == student_id).one_or_none()
-            if student is None:
-                abort(404)
-            else:
-                format_student = student.format
-                return jsonify({
-                    "success": True,
-                    "student": format_student,
-                })
+
+            format_student = student.format
         except Exception:
-            raise sys.exc_info()[1]
+            error = True
+            print(sys.exc_info())
+
+        if error:
+            abort(404)
+        else:
+            return jsonify({
+                "success": True,
+                "student": format_student,
+            })
+
+    @app.route('/students/create', methods=['POST'])
+    def create_student():
+        body = request.get_json()
+        std_fname = body.get('fname')
+        std_lname = body.get('lname')
+        std_email = body.get('email')
+        std_dob = body.get('dob')
+        std_gender = body.get('sex')
+        std_regno = int(body.get('regno'))
+
+        error = False
+
+        try:
+            # instantiate student object
+            std = Student(std_regno, std_fname, std_lname,
+                          std_email, std_dob, std_gender)
+            std.insert()
+            students = Student.query.order_by(Student.std_id).all()
+            students_list = [student.format for student in students]
+
+        except Exception as e:
+            db.session.rollback()
+            error = True
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+        if error:
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'created': std.std_id,
+                'students': students_list,
+                'total': len(students)
+            })
+
+    @app.route('/students/<int:student_id>', methods=['PATCH'])
+    def update_students(student_id):
+        body = request.get_json()
+
+        if body:
+            fname = body.get('fname')
+            lname = body.get('lname')
+            regno = body.get('regno')
+            email = body.get('email')
+        else:
+            abort(400)
+
+        error = False
+
+        try:
+            std = db.session.get(Student, student_id)
+            std.std_fname = fname
+            std.std_lname = lname
+            std.std_regno = regno
+            std.std_email = email
+            std.update()
+        except Exception as e:
+            db.session.rollback()
+            error = True
+            print(sys.exc_info())
+
+        if error:
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'student_id': std.std_id
+            })
+
+    @app.route('/students/<int:student_id>', methods=['DELETE'])
+    def delete_students(student_id):
+        error = False
+
+        try:
+            std = db.session.get(Student, student_id)
+            std.delete()
+
+            updated_students = Student.query.order_by(Student.std_id).all()
+            formatted_students = [
+                student.format for student in updated_students]
+        except Exception as e:
+            db.session.rollback()
+            print(e)
+        finally:
+            db.session.close()
+
+        if error:
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'deleted': std.std_id,
+                'courses': formatted_students,
+                'total_courses': len(updated_students)
+            })
+
+    #---------------------------------------------------------#
+    # Courses
+    #---------------------------------------------------------#
 
     @app.route('/courses')
     @authorize_user('get:data')
     def getCourses():
+        error = False
+
         try:
             courses = db.session.query(Course).order_by(Course.crs_id).all()
             format_courses = [course.format for course in courses]
+
+        except Exception:
+            error = True
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+        if error:
+            abort(404)
+        else:
             return jsonify({
                 "success": True,
                 "courses": format_courses,
-                "total": len(format_courses)
+                "total": len(courses)
             })
-        except Exception as e:
-            # print(sys.exc_info())
-            raise sys.exc_info()[1]
 
-    @app.route('/new_course', methods=['POST'])
-    def post_course():
+    @app.route('/courses/create', methods=['POST'])
+    def create_courses():
         body = request.get_json()
         crs_title = body.get('title')
         crs_code = body.get('code')
@@ -83,6 +209,8 @@ def create_app(test_config=None):
         crs_book = body.get('book')
         crs_lec = int(body.get('lec'))
         crs_room = int(body.get('room'))
+
+        error = False
 
         try:
             # instantiate course object
@@ -92,19 +220,88 @@ def create_app(test_config=None):
             courses = Course.query.order_by(Course.crs_id).all()
             courses_list = [course.format for course in courses]
 
+        except Exception as e:
+            db.session.rollback()
+            error = True
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+        if error:
+            abort(422)
+        else:
             return jsonify({
                 'success': True,
                 'created': crs.crs_id,
                 'courses': courses_list,
                 'total': len(courses)
             })
+
+    @app.route('/courses/<int:course_id>', methods=['PATCH'])
+    def update_courses(course_id):
+        body = request.get_json()
+
+        if body:
+            title = body.get('title')
+            book = body.get('book')
+        else:
+            abort(400)
+
+        error = False
+
+        try:
+            crs = db.session.get(Course, course_id)
+            crs.crs_title = title
+            crs.crs_book = book
+            crs.update()
         except Exception as e:
             db.session.rollback()
-            abort(422)
-            # print(e)
-            # print(sys.exc_info()[1])
+            error = True
+            print(sys.exc_info())
 
-    @ app.errorhandler(404)
+        if error:
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'course_id': crs.crs_id
+            })
+
+    @app.route('/courses/<int:course_id>', methods=['DELETE'])
+    def delete_courses(course_id):
+        error = False
+
+        try:
+            crs = db.session.get(Course, course_id)
+            crs.delete()
+
+            updated_courses = Course.query.order_by(Course.crs_id).all()
+            formatted_courses = [course.format for course in updated_courses]
+        except Exception as e:
+            db.session.rollback()
+            print(e)
+        finally:
+            db.session.close()
+
+        if error:
+            abort(422)
+        else:
+            return jsonify({
+                'success': True,
+                'deleted id': crs.crs_id,
+                'courses': formatted_courses,
+                'total_courses': len(updated_courses)
+            })
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "Success": False,
+            "error": 400,
+            "message": "Bad Request"
+        }), 400
+
+    @app.errorhandler(404)
     def not_found(error):
         return jsonify({
             "Success": False,
@@ -112,7 +309,7 @@ def create_app(test_config=None):
             "message": "Resource Not Found"
         }), 404
 
-    @ app.errorhandler(500)
+    @app.errorhandler(500)
     def server_error(error):
         return jsonify({
             "Success": False,
@@ -120,7 +317,7 @@ def create_app(test_config=None):
             "message": "Internal Server error"
         }), 500
 
-    @ app.errorhandler(422)
+    @app.errorhandler(422)
     def unprocessable(error):
         return jsonify({
             "Success": False,
